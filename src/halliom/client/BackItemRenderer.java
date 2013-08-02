bc0078b95e10be3a544708d82b989e66b3ab5005
Halliom <johan.jww@hotmail.com>
2013-08-02 11:15:36 +0200

    15min commit

--- src/halliom/client/BackItemRenderer.java
+++ src/halliom/client/BackItemRenderer.java
@@ -1,11 +1,11 @@
 package halliom.client;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -24,14 +24,27 @@ public class BackItemRenderer extends RenderPlayer
 	@Override
 	public void doRender(Entity entity, double d0, double d1, double d2, float f, float f1) 
 	{
+		super.doRender(entity, d0, d1, d2, f, f1);
 		GL11.glPushMatrix();
 		{
 			EntityItem item = new EntityItem(entity.worldObj);
 			item.hoverStart = 0f;
-			item.setEntityItemStack(new ItemStack(Block.stone));
-			item1.doRender(item, 0, 0, 0, 0, 0);
+			item.setEntityItemStack(new ItemStack(Item.swordDiamond));
+			
+			GL11.glScalef(1.2F, 1.2F, 1.2F);
+			
+			rotateItemAccordingToPlayer((EntityPlayer)entity);
+			
+			item1.doRender(item, 0, -0.6, 0.2, 1, 1);
 		}
 		GL11.glPopMatrix();
+	}
+	
+	private void rotateItemAccordingToPlayer(EntityPlayer player)
+	{
+		System.out.println(player.rotationYaw);
+		
+		GL11.glRotatef(90, 1, 0, 0);
 	}
 
 }
