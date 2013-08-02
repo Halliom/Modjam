9d9193e59d2df461311f7bfa3bb89e6ac4b0db4f
Halliom <johan.jww@hotmail.com>
2013-08-02 22:28:39 +0200

    Backpack rendering

--- src/halliom/client/BackItemRenderer.java
+++ src/halliom/client/BackItemRenderer.java
@@ -2,12 +2,15 @@ package halliom.client;
 
 import halliom.common.backpack.BackpackData;
 import halliom.common.backpack.BackplateContainer;
+import halliom.common.item.ItemBackpack;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
 
 import org.lwjgl.opengl.GL11;
 
@@ -16,6 +19,8 @@ public class BackItemRenderer extends RenderPlayer
 	
 	private RenderItem item1Renderer = new RenderItem();
 	private RenderItem item2Renderer = new RenderItem();
+	
+	private IModelCustom backPackModel = AdvancedModelLoader.loadModel("/Backpack.obj");
 	
 	public BackItemRenderer() 
 	{
@@ -39,20 +44,27 @@ public class BackItemRenderer extends RenderPlayer
 			GL11.glScalef(1.2F, 1.2F, 1.2F);
 			rotateItemAccordingToPlayer((EntityPlayer)entity);
 			
-			if (container.getLeftItem() != null)
+			if (container.getLeftItem().getItem() instanceof ItemBackpack)
 			{
-				EntityItem item1 = new EntityItem(entity.worldObj);
-				item1.hoverStart = 0f;
-				item1.setEntityItemStack(container.getLeftItem());
-				item1Renderer.doRender(item1, 0, -0.6, -0.15, 1, 1);
+				backPackModel.renderAll();
 			}
-			if (container.getRightItem() != null)
+			else
 			{
-				EntityItem item2 = new EntityItem(entity.worldObj);
-				item2.hoverStart = 0f;
-				item2.setEntityItemStack(container.getRightItem());
-				GL11.glRotatef(270, 0, 0, 1);
-				item2Renderer.doRender(item2, 0.4, -0.2, -0.2, 1, 1);
+				if (container.getLeftItem() != null)
+				{
+					EntityItem item1 = new EntityItem(entity.worldObj);
+					item1.hoverStart = 0f;
+					item1.setEntityItemStack(container.getLeftItem());
+					item1Renderer.doRender(item1, 0, -0.6, -0.15, 1, 1);
+				}
+				if (container.getRightItem() != null)
+				{
+					EntityItem item2 = new EntityItem(entity.worldObj);
+					item2.hoverStart = 0f;
+					item2.setEntityItemStack(container.getRightItem());
+					GL11.glRotatef(270, 0, 0, 1);
+					item2Renderer.doRender(item2, 0.4, -0.2, -0.2, 1, 1);
+				}
 			}
 		}
 		GL11.glPopMatrix();
