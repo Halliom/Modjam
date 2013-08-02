35f23e2e65246340c242bbd9006b5aeca287fdbc
Halliom <johan.jww@hotmail.com>
2013-08-02 16:33:49 +0200

    15min commit

--- src/halliom/common/backpack/BackplateContainer.java
+++ src/halliom/common/backpack/BackplateContainer.java
@@ -2,6 +2,7 @@ package halliom.common.backpack;
 
 import halliom.common.item.ItemBackpack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
 
 
 public class BackplateContainer 
@@ -11,6 +12,33 @@ public class BackplateContainer
 	
 	public ItemStack performSwitch(int pos, ItemStack item)
 	{
-		if (item.getItem() instanceof ItemBackpack)
+		if (item.getItem() instanceof ItemTool)
+		{
+			if (item.getItem() instanceof ItemBackpack)
+				if (inventory[0] == null && inventory[1] == null)
+				{
+					inventory[0] = item;
+					return null;
+				}
+				else
+					return item;
+			else
+			{
+				if (inventory[0].getItem() instanceof ItemBackpack)
+				{
+					inventory[pos] = item;
+					return inventory[0];
+				}
+				else
+				{
+					ItemStack res = inventory[pos];
+					inventory[pos] = item;
+					return res;
+				}
+			}
+		}
+		else
+			return item;
 	}
+	
 }
