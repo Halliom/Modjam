fc5574646d593ef55095830a373d16ad64b95f1f
Halliom <johan.jww@hotmail.com>
2013-08-03 10:18:02 +0200

    commit

--- autocommit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM ---- Locate git.exe from the GIT installation directory. ----
-set GIT_PATH="D:\Johan\Programs\Git\bin\git.exe"
-%GIT_PATH% add -A
-%GIT_PATH% commit -m "Auto-committed on %date%"
-%GIT_PATH% push
