00312862a5d29412bac8ed23ed7da75fc6e917d6
Halliom <johan.jww@hotmail.com>
2013-08-03 16:36:50 +0200

    commit

--- src/halliom/client/gui/special/Gui3D.java
+++ src/halliom/client/gui/special/Gui3D.java
@@ -8,16 +8,19 @@ import java.util.ArrayList;
 public class Gui3D 
 {
 	
+	private Vector3f[] rotations = new Vector3f[]{new Vector3f(0,0,1), new Vector3f(1,0,0), new Vector3f(0,0,-1), new Vector3f(-1,0,0)};
+	private Vector3f pos;
+	
 	private ArrayList<GuiObject3D> objects = new ArrayList<GuiObject3D>();
 	
 	public Gui3D(int face) 
 	{
 		objects.add(new GuiImage3D(new Box(new Vector3f(0,1,0), new Vector3f(1,1,1)), "textures/gui/container/generic_54.png"));
+		pos = rotations[face];
 	}
 	
 	public void handleClick(Vector3f lookVec, int face)
 	{
-		
 		for (GuiObject3D comp : objects)
 		{
 		}
@@ -27,7 +30,7 @@ public class Gui3D
 	{
 		for (GuiObject3D comp : objects)
 		{
-			comp.render(x, y, z, f);
+			comp.render(x * pos.getX(), y * pos.getY(), z * pos.getZ(), f);
 		}
 	}
 	
--- src/halliom/core/ClickEventHandler.java
+++ src/halliom/core/ClickEventHandler.java
@@ -2,6 +2,7 @@ package halliom.core;
 
 import halliom.client.gui.special.Gui3DHandler;
 import halliom.core.util.Vector3f;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
@@ -14,13 +15,15 @@ public class ClickEventHandler
 	{
 		if (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK)
 		{
+			int face = MathHelper.floor_double(event.entityPlayer.rotationYaw * 4f / 360f + 0.5d) & 3;
+			System.out.println(face);
 			if (event.entityPlayer.openContainer == null && Gui3DHandler.has3DGuiOpen(event.entityPlayer))
 			{
 				float lookDist = 4f;
 				Vector3f lookVec = new Vector3f(event.entityPlayer.getLookVec());
 				Vector3f real = lookVec.mul(lookDist).add(new Vector3f((float) event.entityPlayer.posX, (float) event.entityPlayer.posY, (float) event.entityPlayer.posZ));
 				
-				Gui3DHandler.getOpenGui(event.entityPlayer).handleClick();
+				Gui3DHandler.getOpenGui(event.entityPlayer).handleClick(lookVec, event.face);
 			}
 		}
 	}
